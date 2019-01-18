7faff7e76f0f80eedeffdf4b48cce7bdbfcfa1a7
anupamsingh194 <anupamsingh194@gmail.com>
2019-01-17 17:49:10 -0800

    Update oceans_islands_in_fasta.py

--- oceans_islands_in_fasta.py
+++ oceans_islands_in_fasta.py
@@ -1,12 +1,9 @@
-##islands=gaps, oceans=NNN --------------> Code to find oceans and islands
+##islands=NNN, oceans=ATGC,
 import collections
 import re
 seq_list={}
 key=""
 gap= 1
-no_gap=1
-
-
 with open("/Users/anupamsi/Downloads/test.fasta","r") as fasta_seq:
     with open("/Users/anupamsi/Downloads/test_subset_CDCFrontier.txt","w") as fasta_seq_with_gaps:
         for line in fasta_seq:
@@ -22,19 +19,17 @@ with open("/Users/anupamsi/Downloads/test.fasta","r") as fasta_seq:
 
         for real_key, value in seq_list.iteritems():
             value=''.join(value[1:])
-            seq_list[real_key]=value
-            
-            islands_Dictionary =({real_key : [[real_key,(m.start(1))+1, (m.end()-1)+1] for m in re.finditer('(N+)', value)]})
-            oceans_Dictionary =({real_key : [[real_key,(m.start(1))+1, (m.end()-1)+1] for m in re.finditer('([ATGC]+)', value)]})
-            for real_key1, value1 in oceans_Dictionary.iteritems():
-                for i, element1 in enumerate(value1):
-                    value1[i].append('S'+(str(no_gap).zfill(5)))
-                    no_gap+=1
-                    print value1[i]
+            seq_list[real_key]=value            
+            islands_Dictionary =({real_key : [[real_key,(m.start(1))+1, (m.end()-1)+1] for m in re.finditer('(N+)', value)]})            
+            oceans_Dictionary =({real_key : [[real_key,(m.start(1))+1, (m.end()-1)+1] for m in re.finditer('([ATGC]+)', value)]})            
+            for (real_key1, value1), (real_key2, value2) in zip(oceans_Dictionary.iteritems(), islands_Dictionary.iteritems()):
+                lenght_of_list=(len(value1))
+                for i in range(0,(lenght_of_list)-1):
+                    value1[i].append('S'+(str(gap).zfill(5)))
                     fasta_seq_with_gaps.write('\t'.join(str(s) for s in value1[i]) + '\n')
-            for real_key2, value2 in islands_Dictionary.iteritems():
-                for j, element in enumerate(value2):
-                    value2[j].append('G'+(str(gap).zfill(5)))
-                    gap+=1
-                    print value2[j]
-                    fasta_seq_with_gaps.write('\t'.join(str(s) for s in value2[j]) + '\n')
+                    value2[i].append('G'+(str(gap).zfill(5)))
+                    fasta_seq_with_gaps.write('\t'.join(str(s) for s in value2[i]) + '\n')
+                    gap+=1                  
+                value1[lenght_of_list-1].append('S'+(str(gap).zfill(5)))
+                fasta_seq_with_gaps.write('\t'.join(str(s) for s in value1[lenght_of_list-1]) + '\n')
+                gap+=1
